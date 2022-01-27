1924cd54538eff2c603bad65f7fb29518f0c6edb
zgrkbr <ozgurkibar78@gmail.com>
2022-01-27 11:16:44 +0100

    add Swagger class

---
+++ sti-spring-boot-swagger-endpoint/src/main/java/sti/spring/boot/swagger/endpoint/Swagger.java
@@ -0,0 +1,13 @@
+package sti.spring.boot.swagger.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger.class, args);
+    }
+
+}
